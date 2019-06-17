3dfbfd8c7c75aa56fb1dd63608b895fabfe80463
eleele <2369745961@qq.com>
2019-06-17 22:16:19 +0800

    emm

--- index.js
+++ index.js
@@ -1,105 +1,105 @@
 var cm
-const argv={}
-location.search.substr(1).split('&').forEach(str=>{
-	const a=str.split('=')
-	argv[a[0]]=a[1]
+const argv = {}
+location.search.substr(1).split('&').forEach(str => {
+	const a = str.split('=')
+	argv[a[0]] = a[1]
 })
 console.log(argv)
-function utoa(s){
+function utoa(s) {
 	return btoa(unescape(encodeURIComponent(s)))
 }
-function atou(s){
+function atou(s) {
 	return decodeURIComponent(escape(atob(s)))
 }
-function encode(s){
-	return utoa(s).replace(/\+/g,'-').replace(/\//g,'_').replace(/=/g,'').replace(/jb/,'j`b')
+function encode(s) {
+	return utoa(s).replace(/\+/g, '-').replace(/\//g, '_').replace(/=/g, '').replace(/jb/, 'j`b')
 }
-function decode(s){
-	return atou(s.replace(/_/g,'/').replace(/-/g,'+').replace(/`/g,''))
+function decode(s) {
+	return atou(s.replace(/_/g, '/').replace(/-/g, '+').replace(/`/g, ''))
 }
-const editor={
-	template:'#editorTpl',
-	props:['value'],
-	methods:{
+const editor = {
+	template: '#editorTpl',
+	props: ['value'],
+	methods: {
 	},
-	mounted(){
-		cm=this.cm=CodeMirror.fromTextArea(document.getElementById('blabla'),{
-			lineNumbers:true,
-			theme:'monokai',
-			mode:argv.lang || 'javascript'
+	mounted() {
+		cm = this.cm = CodeMirror.fromTextArea(document.getElementById('blabla'), {
+			lineNumbers: true,
+			theme: 'monokai',
+			mode: argv.lang || 'javascript'
 		})
-		this.cm.on('change',()=>{
-			this.value=this.cm.getValue()
-			this.$emit('input',this.value)
+		this.cm.on('change', () => {
+			this.value = this.cm.getValue()
+			this.$emit('input', this.value)
 		})
 		if (argv.code) this.cm.setValue(decode(argv.code))
 	}
 }
-Vue.component('editor',editor)
-const vm=new Vue({
-	el:'#app',
-	data:{
-		code:'',
-		id:'fsd',
-		lang:argv.lang || 'javascript',
-		showMenu:false,
-		beginTime:new Date(),
-		consoleFullscreen:false,
-		fileCnt:0,
-		fileList:[]
+Vue.component('editor', editor)
+const vm = new Vue({
+	el: '#app',
+	data: {
+		code: '',
+		id: 'fsd',
+		lang: argv.lang || 'javascript',
+		showMenu: false,
+		beginTime: new Date(),
+		consoleFullscreen: false,
+		fileCnt: 0,
+		fileList: []
 	},
-	methods:{
-		run(){
-			const req={
-				lang:this.lang,
-				code:this.code,
-				token:argv.token||''
+	methods: {
+		run() {
+			const req = {
+				lang: this.lang,
+				code: this.code,
+				token: argv.token || ''
 			}
-			this.sandBox.postMessage(JSON.stringify(req),'*')
+			this.sandBox.postMessage(JSON.stringify(req), '*')
 		},
-		changLang(){
-			cm.setOption('mode',this.lang)
+		changLang() {
+			cm.setOption('mode', this.lang)
 		},
-		loadFiles(){
-			this.fileCnt=0
-			this.fileList=[]
-			for (var i=0; i<localStorage.length; ++i){
-				const key=localStorage.key(i)
-				if (key.indexOf('file_')==0){
+		loadFiles() {
+			this.fileCnt = 0
+			this.fileList = []
+			for (var i = 0; i < localStorage.length; ++i) {
+				const key = localStorage.key(i)
+				if (key.indexOf('file_') == 0) {
 					++this.fileCnt
 					this.fileList.push(key.substr(5))
 				}
 			}
 		},
-		loadFile(i){
-			cm.setValue(localStorage.getItem('file_'+this.fileList[i]))
+		loadFile(i) {
+			cm.setValue(localStorage.getItem('file_' + this.fileList[i]))
 		},
-		saveFile(){
-			const fileName=prompt('File name:')
+		saveFile() {
+			const fileName = prompt('File name:')
 			if (!fileName) return
-			localStorage.setItem('file_'+fileName,this.code)
+			localStorage.setItem('file_' + fileName, this.code)
 			this.loadFiles()
 		},
-		deleteFile(){
-			const fileName=prompt('File name:')
+		deleteFile() {
+			const fileName = prompt('File name:')
 			if (!fileName) return
-			localStorage.removeItem('file_'+fileName)
+			localStorage.removeItem('file_' + fileName)
 			this.loadFiles()
 		},
-		chkLongTap(){
-			var T=new Date()-this.beginTime
-			if (T>=300){
+		chkLongTap() {
+			var T = new Date() - this.beginTime
+			if (T >= 300) {
 				// alert(T)
-				this.consoleFullscreen=!this.consoleFullscreen
+				this.consoleFullscreen = !this.consoleFullscreen
 			}
-			else this.showMenu=!this.showMenu
+			else this.showMenu = !this.showMenu
 		}
 	},
-	mounted(){
-		this.sandBox=document.getElementById(this.id).contentWindow
+	mounted() {
+		this.sandBox = document.getElementById(this.id).contentWindow
 		this.loadFiles()
 	}
 })
-if ('serviceWorker' in navigator){
+if ('serviceWorker' in navigator) {
 	navigator.serviceWorker.register('sw.js')
 }
